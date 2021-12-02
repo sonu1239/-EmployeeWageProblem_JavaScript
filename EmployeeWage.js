96e29a363c39e0e78567a4610e05785df1b71910
sonu1239 <ss316742@gmail.com>
2021-12-02 12:39:54 +0530

    added for calculate monthly employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,6 +2,7 @@
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
 const WAGE_PER_HOURS = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 //This function for return working hours
 function getWorkingHours(empCheck)
@@ -18,10 +19,15 @@ function getWorkingHours(empCheck)
 }
 
 let empHrs = 0;
-//gives random value
-let empCheck = Math.floor(Math.random() * 10) % 3;
-//calling function
-empHrs = getWorkingHours(empCheck);
+
+for (let day=0; day<NUM_OF_WORKING_DAYS; day++)
+{
+    //gives random value
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    //calling function
+    empHrs += getWorkingHours(empCheck);
+}
+
 //calculating employee wage
 let empWage = empHrs * WAGE_PER_HOURS;
-console.log("Employee Wage: " + empWage);
+console.log("Total Hours: " +empHrs+"\nEmployee Monthly Wage: " + empWage);
